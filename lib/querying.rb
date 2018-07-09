b5af213730692f9a798a1e261c9124f1039fa4b5
Taylor Williams <taylor@thewilliams.net>
2018-07-09 23:25:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+  INNER JOIN books ON books.series_id = series.id
+  INNER JOIN character_books ON character_books.book_id = books.id
+  INNER JOIN characters ON character_id = character_books.character_id
+  GROUP BY characters.species
+  HAVING 'human'
+  ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
