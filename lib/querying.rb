319bbea9afa558bb868a8decd698f8519988be4d
Taylor Williams <taylor@thewilliams.net>
2018-07-10 00:21:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title, characters.species FROM series
   INNER JOIN characters ON characters.series_id = series.id
   ORDER BY COUNT(characters.species) DESC LIMIT 1
-  where characters.species = 'human'"
+  WHERE characters.species = 'human'"
 end
 
 def select_character_names_and_number_of_books_they_are_in
